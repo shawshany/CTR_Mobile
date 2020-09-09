c5c0741bd13793118d67e6d2405d1b7728f6381e
xiaosheng.yxs <xiaosheng.yxs@alibaba-inc.com>
2020-09-09 19:52:19 +0800

    add main

---
+++ main/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*-coding:utf-8 -*-
+#Reference:**********************************************
+# @Time     : 2020-09-09 19:52
+# @Author   : ora
+# @E-mail   : victor.xsyang@gmail.com
+# @File     : __init__.py.py
+# @User     : ora
+# @Software: PyCharm
+# @Description: 
+#Reference:**********************************************
